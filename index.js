121296127f6db630ada5552221f3064be61e6cf6
DanielZemberi <daniel.zemberi@sensoneo.com>
2023-12-22 20:58:13 +0100

    fix: chrome executable path

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get("/", async (req, res) => {
   console.log("Starting script");
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: '/usr/bin/google-chrome',
+    executablePath: '/usr/bin/chromium-browser',
     args: [
         "--no-sandbox",
         "--disable-gpu",
